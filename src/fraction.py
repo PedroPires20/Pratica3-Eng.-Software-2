01c432eef5e3ea01e3fd237154e9da3003213358
Pedro Dias <pedrodias504@gmail.com>
2022-06-16 21:39:08 -0300

    Corrigindo erro com os typehints

--- src/fraction.py
+++ src/fraction.py
@@ -9,7 +9,7 @@ class Fraction:
         self._sign = sign(numerator) * sign(denominator)
     
     # Retorna a fração inversa da atual
-    def inverse(self) -> Fraction:
+    def inverse(self) -> 'Fraction':
         return Fraction(self._denominator, self._numerator)
     
     # Retorna o valor do numerador da fração atual
@@ -29,55 +29,55 @@ class Fraction:
         return (self._numerator / self._denominator) * self._sign
     
     # Retorna a forma simplificada (irredutível) da fração atual
-    def simplify(self) -> Fraction:
+    def simplify(self) -> 'Fraction':
         divisor = mdc(self._numerator, self._denominator)
         return Fraction(self._numerator // divisor, self._denominator // divisor)
     
     # Operador para calcular o oposto da fração atual
-    def __neg__(self) -> Fraction:
+    def __neg__(self) -> 'Fraction':
         return Fraction(-self._numerator, self._denominator)
 
     # Operador para adição de duas frações
-    def __add__(self, f2: Fraction) -> Fraction:
+    def __add__(self, f2: 'Fraction') -> 'Fraction':
         sum_numerator = (self._numerator * f2._denominator) + (f2._numerator * self._denominator)
         sum_denominator = self._denominator * f2._denominator
         return Fraction(sum_numerator, sum_denominator)
     
     # Operador para subtração de duas frações
-    def __sub__(self, f2: Fraction) -> Fraction:
+    def __sub__(self, f2: 'Fraction') -> 'Fraction':
         sum_numerator = (self._numerator * f2._denominator) - (f2._numerator * self._denominator)
         sum_denominator = self._denominator * f2._denominator
         return Fraction(sum_numerator, sum_denominator)
 
     # Operador para multiplicação de duas frações
-    def __mul__(self, f2: Fraction) -> Fraction:
+    def __mul__(self, f2: 'Fraction') -> 'Fraction':
         result_sign = self._sign * f2._sign
         return Fraction(self._numerator * f2._numerator * result_sign, self._denominator * f2._denominator)
     
     # Operador para divisão de duas frações
-    def __div__(self, f2: Fraction) -> Fraction:
+    def __truediv__(self, f2: 'Fraction') -> 'Fraction':
         return self * f2.inverse()
 
     # Operador para verificar se duas frações são iguais
-    def __eq__(self, f2: Fraction) -> bool:
+    def __eq__(self, f2: 'Fraction') -> bool:
         return (self._sign == f2._sign) and (self._numerator == f2._numerator) and (self._denominator == f2._denominator)
 
     # Operador para verificar se duas frações são diferentes
-    def __neq__(self, f2: Fraction) -> bool:
+    def __neq__(self, f2: 'Fraction') -> bool:
         return not (self == f2)
     
     # Operador para verificar se a fração atual é menor que a fração dada
-    def __lt__(self, f2: Fraction) -> bool:
+    def __lt__(self, f2: 'Fraction') -> bool:
         return self.to_float() < f2.to_float()
     
     # Operador para verificar se a fração atual é maior que a fração dada
-    def __gt__(self, f2: Fraction) -> bool:
+    def __gt__(self, f2: 'Fraction') -> bool:
         return self.to_float() > f2.to_float()
     
     # Operador para verificar se a fração atual é menor ou igual que a fração dada
-    def __lte__(self, f2: Fraction) -> bool:
+    def __lte__(self, f2: 'Fraction') -> bool:
         return (self < f2) or (self == f2)
     
     # Operador para verificar se a fração atual é maior ou igual que a fração dada
-    def __gte__(self, f2: Fraction) -> bool:
+    def __gte__(self, f2: 'Fraction') -> bool:
         return (self > f2) or (self == f2)
