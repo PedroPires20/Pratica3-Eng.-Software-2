656738c8ad272a94987025f7a2d7015b3a84ef22
Pedro Dias <pedrodias504@gmail.com>
2022-06-17 19:30:48 -0300

    Adicionando mais testes para a classe Fraction

--- tests/test_fraction.py
+++ tests/test_fraction.py
@@ -61,3 +61,73 @@ class FractionTest(TestCase):
         self.assertEqual(1, f2.sign())
         self.assertEqual(25, f2.numerator())
         self.assertEqual(2, f2.denominator())
+
+    def test_fraction_add(self):
+        f1 = Fraction(1, 2)
+        f2 = Fraction(2, 3)
+        res = f1 + f2
+        self.assertEqual(1, res.sign())
+        self.assertEqual(7, res.numerator())
+        self.assertEqual(6, res.denominator())
+    
+    def test_fraction_sub(self):
+        f1 = Fraction(1, 4)
+        f2 = Fraction(1, 3)
+        res = f1 - f2
+        self.assertEqual(-1, res.sign())
+        self.assertEqual(1, res.numerator())
+        self.assertEqual(12, res.denominator())
+    
+    def test_fraction_mul(self):
+        f1 = Fraction(-2, 5)
+        f2 = Fraction(-5, 3)
+        res = f1 * f2
+        self.assertEqual(1, res.sign())
+        self.assertEqual(2, res.numerator())
+        self.assertEqual(3, res.denominator())
+    
+    def test_fraction_div(self):
+        f1 = Fraction(1, 4)
+        f2 = Fraction(-3, 2)
+        res = f1 / f2
+        self.assertEqual(-1, res.sign())
+        self.assertEqual(1, res.numerator())
+        self.assertEqual(6, res.denominator())
+    
+    def test_fraction_equals(self):
+        f1 = Fraction(1, 2)
+        f2 = Fraction(1, 3)
+        self.assertTrue(f1 == f1)
+        self.assertFalse(f1 == f2)
+    
+    def test_fraction_not_equals(self):
+        f1 = Fraction(1, 2)
+        f2 = Fraction(1, 3)
+        self.assertTrue(f1 != f2)
+        self.assertFalse(f1 != f1)
+    
+    def test_fraction_greater(self):
+        f1 = Fraction(1, 3)
+        f2 = Fraction(1, 4)
+        self.assertTrue(f1 > f2)
+        self.assertFalse(f2 > f1)
+    
+    def test_fraction_less(self):
+        f1 = Fraction(1, 4)
+        f2 = Fraction(2, 3)
+        self.assertTrue(f1 < f2)
+        self.assertFalse(f2 < f1)
+    
+    def test_fraction_greater_equal(self):
+        f1 = Fraction(10, 3)
+        f2 = Fraction(5, 4)
+        self.assertTrue(f1 >= f2)
+        self.assertTrue(f1 >= f1)
+        self.assertFalse(f2 >= f1)
+    
+    def test_fraction_less_equal(self):
+        f1 = Fraction(4, 3)
+        f2 = Fraction(5, 2)
+        self.assertTrue(f1 < f2)
+        self.assertTrue(f1 <= f1)
+        self.assertFalse(f2 < f1)
