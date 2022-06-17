88c5aad89e613f8610506df290fad7958022b317
Pedro Dias <pedrodias504@gmail.com>
2022-06-17 16:33:13 -0300

    Testes de unidade para as funções auxiliares

---
+++ tests/test_auxiliary.py
@@ -0,0 +1,22 @@
+from unittest import TestCase
+from src.auxiliary import sign, mdc
+
+class AuxiliaryTest(TestCase):
+    def test_sign_negative(self):
+        self.assertEqual(-1, sign(-10))
+    
+    def test_sign_positive(self):
+        self.assertEqual(1, sign(0))
+        self.assertEqual(1, sign(5))
+    
+    def test_positive_mdc(self):
+        self.assertEqual(3, mdc(9, 3))
+        self.assertEqual(1, mdc(21, 2))
+    
+    def test_negative_mdc(self):
+        self.assertEqual(2, mdc(10, -2))
+        self.assertEqual(5, mdc(-25, 5))
+    
+    def test_null_mdc(self):
+        self.assertEqual(100, mdc(100, 0))
+        self.assertEqual(20, mdc(0, 20))
